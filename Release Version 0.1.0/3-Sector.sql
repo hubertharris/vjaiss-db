fb402560f686e0ee6470ef36361fdf2622048466
hubert <hubertharris.m@gmail.com>
2024-03-03 15:02:01 +0000

    All sectors and Industries from finviz is commited during first release, This is a belated commit after it is forgotten for long time

--- Release Version 0.1.0/3-Sector.sql
+++ Release Version 0.1.0/3-Sector.sql
@@ -6,3 +6,15 @@ CREATE TABLE `sector` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB;
+
+INSERT INTO `sector`(`name`) VALUES('Basic Materials');
+INSERT INTO `sector`(`name`) VALUES('Communication Services');
+INSERT INTO `sector`(`name`) VALUES('Consumer Cyclical');
+INSERT INTO `sector`(`name`) VALUES('Consumer Defensive');
+INSERT INTO `sector`(`name`) VALUES('Energy');
+INSERT INTO `sector`(`name`) VALUES('Financial');
+INSERT INTO `sector`(`name`) VALUES('Healthcare');
+INSERT INTO `sector`(`name`) VALUES('Industrials');
+INSERT INTO `sector`(`name`) VALUES('Real Estate');
+INSERT INTO `sector`(`name`) VALUES('Technology');
+INSERT INTO `sector`(`name`) VALUES('Utilities');
